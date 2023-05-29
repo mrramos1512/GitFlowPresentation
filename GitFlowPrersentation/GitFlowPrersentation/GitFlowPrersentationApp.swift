67e337d567aef11a0d16864214193eb9075e4216
Leandro Antonio dos Anjos Ramos <i424534@MacBook-Pro-de-DTI.local>
2023-05-29 10:09:12 -0300

    initial commit

---
+++ GitFlowPrersentation/GitFlowPrersentation/GitFlowPrersentationApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitFlowPrersentationApp.swift
+//  GitFlowPrersentation
+//
+//  Created by Leandro Antonio dos Anjos Ramos on 29/05/23.
+//
+
+import SwiftUI
+
+@main
+struct GitFlowPrersentationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
